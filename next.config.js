0ccd54bd001eea7fe45b0aa374ff70b1110e8379
Lisdexico96 <147284689+Lisdexico96@users.noreply.github.com>
2026-03-04 13:52:53 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Enable static export so the app can be hosted on GitHub Pages
+  output: "export",
 };
 
 module.exports = nextConfig;
